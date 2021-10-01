1c9b3a9a8f9b5bc613ffc367183a92907950ed5b
filakkin <filakkin@gmail.com>
2021-10-01 22:19:35 +0700

    Добавит "ручки" с заглушками требуемого API

--- src/server.js
+++ src/server.js
@@ -2,10 +2,49 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
+app.get('/list', (req, res) => {
+    res.json(
+        [
+            {
+                id: 'random-id',
+                size: 123412,
+                uploadedAt: 1576767673
+            },
+            {
+                id: 'another-random-id',
+                size: 253265,
+                uploadedAt: 15248125812
+            }
+        ]
+    );
+});
+
+app.get('/image/:id', (req, res) => {
+    res.json(
+        {
+            text: `Started download of image with id ${req.params.id}`
+        }
+    );
+});
+
+app.get('/merge', (req, res) => {
+    res.json(req.query);
+});
+
 app.get('/', (req, res) => {
     res.send('Hello world!');
 });
 
+app.post('/upload', (req, res) => {
+    console.log(req.body)
+    res.send('uploaded');
+});
+
+app.delete('/image/:id', (req, res) => {
+    res.send(`deleted ${req.params.id}`);
+});
+
 app.listen(port, () => {
-    console.log(`Listening at http://localhost:${port}`);
+    console.log(process.argv);
+    console.log(`Listening for req at http://localhost:${port}`);
 });
