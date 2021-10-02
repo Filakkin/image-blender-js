4a04c6125b8735d24ef3804376d4f9d59f447dbe
filakkin <filakkin@gmail.com>
2021-10-03 02:08:01 +0700

    Добавит слияние картинок

--- src/api/merge.js
+++ src/api/merge.js
@@ -1,5 +1,21 @@
+import { replaceBackground } from 'backrem';
+import { createReadStream } from 'fs';
+
+import imageService from '../service/imageService.js';
+
 const merge = (req, res) => {
-    res.json(req.query);
+    const frontImagePath = imageService.getImagePath(req.query.front);
+    const backImagePath = imageService.getImagePath(req.query.back);
+
+    const color = req.query.color.split(',').map((value) => parseInt(value, 10))
+    const threshold = parseInt(req.query.threshold, 10);
+
+    res.type('jpeg')
+
+    console.log(`front image: ${frontImagePath} back image: ${backImagePath}`)
+    replaceBackground(createReadStream(frontImagePath), createReadStream(backImagePath), color, threshold)
+    .then((readStrem) => readStrem.pipe(res));
 }
+
 
 export { merge };
